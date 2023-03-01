fd1ed6dfc56aa8414a91ce61fe4022bf4470ec14
Pathce <asfgzx@naver.com>
2023-03-01 18:18:03 +0900

    UPDATE : H2 연동
    JPA 를 이용해 H2 연결하는 샘플과. 테스트코드 작성

---
+++ demo/src/main/java/com/example/demo/Member.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+
+}
